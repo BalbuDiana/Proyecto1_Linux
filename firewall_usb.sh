3685170cf200f27f4c50d516b510b1a2b28061d3
Balbuena, Diana <dianabalbuenavalencia@outlook.com>
2017-08-27 09:45:03 -0500

    Comentarios
    Se comenta la parte con la que se intentó demonizar puesto que no deja un buen funcionamiento en el programa

--- firewall_usb.sh
+++ firewall_usb.sh
@@ -7,17 +7,23 @@
 echo "Hi $USER!"
 echo "You're run : $0 program"
 
-CONTROL=0
-while [ $CONTROL=0 ] ; do
+#CONTROL=0
+#while [ $CONTROL=0 ] ; do
+	echo "test"
         #Creacion de nuestras listas blanca y negra donde pondremos el nombre de las usb's
         touch WhiteList.txt | touch BlackList.txt
         cat /etc/mtab | grep media >> /dev/null
-        if [ $? -ne 0 ]; then
+	echo "test 2"
+        #if [ $? -ne 0 ]; then
+		echo "test 3"
                 CONTROL=0
-        else
+        #else
+		echo "test 4"
                 CONTROL=1
-                for USBDEV in `df | grep media | awk -F / {'print $5'}` ; do
-                        echo "Se a conectado: $USBDEV" 
+         #       for USBDEV in `df | grep media | awk -F / {'print $5'}` ; do
+			echo "test 5"
+                        echo "Se a conectado: $USBDEV"
+			echo "test 6" 
                         echo "1)Montar y Ejecutar 2)Añadir a la lista Blanca 3)Añadir a la lista Negra 4)Desmontar"
                         read eleccion
                                 case $eleccion in
@@ -34,8 +40,8 @@ while [ $CONTROL=0 ] ; do
 						CONTROL=1;;
 					*) echo "opcion invalida";;
 				esac
-				break;
-		done
-        fi                 
-done
+	#			break;
+	#	done
+        #fi                 
+#done
 exit 0
