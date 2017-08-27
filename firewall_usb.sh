53369b7fab804c80b6390ce1b54fa67ea052a91b
Julio César Martínez <31251600+joule7@users.noreply.github.com>
2017-08-27 14:25:53 -0500

    definicion de varible
    cambio del lugar de la variable y quitando ciclo for

--- firewall_usb.sh
+++ firewall_usb.sh
@@ -12,30 +12,30 @@ while [ $CONTROL=0 ] ; do
         #Creacion de nuestras listas blanca y negra donde pondremos el nombre de las usb's
         touch WhiteList.txt | touch BlackList.txt
         cat /etc/mtab | grep media >> /dev/null
+	#Declaracion de la barible USB
+	USB=$( dmesg | tail -n20 | grep ": Serial" | awk '{print $6}' )
         if [ $? -ne 0 ]; then
                 CONTROL=0
         else
                 CONTROL=1
-                for USBDEV in `df | grep media | awk -F / {'print $5'}` ; do
-                        echo "Se a conectado: $USBDEV" 
-                        echo "1)Montar y Ejecutar 2)Añadir a la lista Blanca 3)Añadir a la lista Negra 4)Desmontar"
-                        read eleccion
-                                case $eleccion in
-					1) echo $USBDEV >> WhiteList.txt
-						mount /dev/sdc1
-                                                sudo eject /dev/sdc1
-						CONTROL=1;;
-                                        2) echo $USBDEV >> WhiteList.txt
-						CONTROL=1;;
-					3) echo $USBDEV >> BlackList.txt
-						CONTROL=1;;
-                                        4) echo $USBDEV >> BlackList.txt
-						mount /dev/sdc1
-						CONTROL=1;;
-					*) echo "opcion invalida";;
-				esac
-				break;
-		done
-        fi                 
+		echo "Se a conectado: $USBDEV" 
+                echo "1)Montar y Ejecutar 2)Añadir a la lista Blanca 3)Añadir a la lista Negra 4)Desmontar"
+		read eleccion
+			case $eleccion in
+				1) echo $USB >> WhiteList.txt
+					mount /dev/sdc1
+					sudo eject /dev/sdc1
+					CONTROL=1;;
+				2) echo $USB >> WhiteList.txt
+					CONTROL=1;;
+				3) echo $USB >> BlackList.txt
+					CONTROL=1;;
+				4) echo $USB >> BlackList.txt
+					mount /dev/sdc1
+					CONTROL=1;;
+				*) echo "opcion invalida";;
+			esac
+			break;
+	fi                 
 done
 exit 0
